ea0e0faeee0caf2167f68fae21eec18004a5f3da
Dharmik Dudhat <dharmikdudhat8@gmail.com>
2023-05-09 18:20:27 +0530

    issue solved

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,SafeAreaView} from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
 import React from 'react'
 import StackNavigation from './src/Navigators/StackNavigation'
 import BottomNavigation from './src/Navigators/BottomNavgation'
@@ -8,11 +8,10 @@ import { store } from './src/Redux/Store'
 const App = () => {
   return (
     <Provider store={store}>
-
-    <StackNavigation/>
+      <StackNavigation />
     </Provider>
-   
-    
+
+
   )
 }
 
